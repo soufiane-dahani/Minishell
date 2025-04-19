d7b27d3989ddb875b7d58ab61d5909d7f25f6955
soufiane-dahani <dahanissoufiane@gmail.com>
2025-04-19 16:13:43 +0100

    first version of exec_external

--- exc/exec_external.c
+++ exc/exec_external.c
@@ -6,9 +6,95 @@
 /*   By: sodahani <sodahani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 16:26:37 by sodahani          #+#    #+#             */
-/*   Updated: 2025/04/19 13:06:58 by sodahani         ###   ########.fr       */
+/*   Updated: 2025/04/19 16:13:06 by sodahani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+char	*check_command_in_paths(char *cmd, char **paths)
+{
+	char	*path;
+	char	*part_path;
+	int		i;
+
+	i = 0;
+	while (paths[i])
+	{
+		part_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part_path, cmd);
+		free(part_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*find_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path;
+	int		i;
+
+	i = 0;
+	while (envp[i] && ft_strnstr(envp[i], "PATH=", 5) == 0)
+		i++;
+	if (!envp[i])
+		return (NULL);
+	paths = ft_split(envp[i] + 5, ':');
+	path = check_command_in_paths(cmd, paths);
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (path);
+}
+
+void execute(char **cmd, char **envp)
+{
+	char *path;
+
+	if (ft_strchr(cmd[0], '/'))
+	{
+		if (access(cmd[0], F_OK | X_OK) == -1)
+			perror("command not found");
+		else
+			execve(cmd[0], cmd, envp);
+		exit(127);
+	}
+	path = find_path(cmd[0], envp);
+	if (!path)
+	{
+		write(2, "command not found\n", 19);
+		exit(127);
+	}
+	execve(path, cmd, envp);
+	perror("execve failed");
+	exit(1);
+}
+
+
+int	exec_external(t_ast *node, char **envp)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		execute(node->cmd, envp);
+		exit(1);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		return (WEXITSTATUS(status));
+	}
+}
