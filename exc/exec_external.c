2f2e4d5a45b4e8295c49dbe28168bd5e4cfbf26b
Yassir Ait Ammi <yaait-am@c4r10p6.1337.ma>
2025-04-28 20:17:07 +0100

    sir tkhedem

--- exc/exec_external.c
+++ exc/exec_external.c
@@ -6,7 +6,7 @@
 /*   By: yaait-am <yaait-am@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 16:26:37 by sodahani          #+#    #+#             */
-/*   Updated: 2025/04/28 20:02:29 by yaait-am         ###   ########.fr       */
+/*   Updated: 2025/04/28 20:15:44 by yaait-am         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,16 +81,10 @@ int	is_root(char *s)
 	int	i;
 
 	i = 0;
-	if (access(s, F_OK | X_OK) == -1)
-		return (127);
-	if (s[i] == '/' || access(s, F_OK | X_OK))
-	{
-		while (s[i] && s[i] == '/')
-			i++;
-		if (!s[i])
-			return (126);
-		return (0);
-	}
+	while (s[i] && s[i] == '/')
+		i++;
+	if (!s[i])
+		return (1);
 	return (0);
 }
 
@@ -100,7 +94,8 @@ int	exec_external(t_ast *node, char **envp)
 	int		status;
 	char	*str;
 
-	(1) && (status = 0), pid = fork();
+	status = 0;
+	pid = fork();
 	reset_signals();
 	setup_execution_signals();
 	if (pid == -1)
@@ -114,9 +109,12 @@ int	exec_external(t_ast *node, char **envp)
 	else
 	{
 		str = ft_strjoin("/usr/bin/", node->cmd[0]);
+		if (access(str, F_OK | X_OK) == -1 && access(node->cmd[0], F_OK | X_OK) == -1)
+			return (waitpid(pid, &status, 0), 127);
 		if (is_root(node->cmd[0]))
-			return (waitpid(pid, &status, 0), is_root(node->cmd[0]));
+			return (waitpid(pid, &status, 0), 126);
 		g_ast->exit_status = exit_status(status);
-		return (waitpid(pid, &status, 0), g_ast->exit_status);
+		waitpid(pid, &status, 0);
+		return (g_ast->exit_status);
 	}
 }
