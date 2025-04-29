24fee765d0a5345caf2bcf4ab5c586fb54a919c7
Yassir Ait Ammi <yaait-am@c4r7p10.1337.ma>
2025-04-29 15:31:22 +0100

    handle her doc

--- exc/exec_redirection_here_doc.c
+++ exc/exec_redirection_here_doc.c
@@ -3,14 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   exec_redirection_here_doc.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sodahani <sodahani@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaait-am <yaait-am@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 16:26:37 by sodahani          #+#    #+#             */
-/*   Updated: 2025/04/27 22:41:09 by sodahani         ###   ########.fr       */
+/*   Updated: 2025/04/29 15:30:35 by yaait-am         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	more_expand(char **new, int *i, char *s, int *a)
+{
+	int		j;
+	char	*store;
+	char	*env;
+
+	j = 0;
+	store = ft_malloc((ft_strlen(s) * sizeof(char)) + 1, FT_ALLOC);
+	(*i)++;
+	while (s[(*i)] && ft_isalnum(s[(*i)]))
+		store [j++] = s[(*i)++];
+	store[j] = '\0';
+	env = getenv(store);
+	if (env)
+	{
+		j = 0;
+		while (env[j])
+			(*new)[(*a)++] = env[j++];
+	}
+}
+
+char	*exp_for_herdoc(char *s)
+{
+	int		i;
+	int		a;
+	char	*env;
+	char	*new;
+
+	i = 0;
+	env = ft_malloc(ft_strlen(s) + 1, FT_ALLOC);
+	a = calculate_s(s, env);
+	new = ft_malloc(a * sizeof(char) + 1, FT_ALLOC);
+	a = 0;
+	while (s[i])
+	{
+		if (s[i] == '$')
+			more_expand(&new, &i, s, &a);
+		else
+			new[a++] = s[i++];
+	}
+	new[a] = '\0';
+	return (new);
+}
 
 int	get_next_line(char **line)
 {
@@ -49,6 +93,8 @@ int	handle_child_process(t_ast *node, int fd[2])
 		if (ft_strncmp(line, node->r->cmd[0], ft_strlen(node->r->cmd[0])) == 0
 			&& ft_strlen(line) == ft_strlen(node->r->cmd[0]))
 			break ;
+		if (node->r->type != TYP_SQOUTE && node->r->type != TYP_DQUOTE)
+			line = exp_for_herdoc(line);
 		write(fd[1], line, ft_strlen(line));
 		write(fd[1], "\n", 1);
 	}
@@ -63,23 +109,48 @@ int	typ_redhere_fun(t_ast *node, char ***envp, t_export_store *store)
 	int		status;
 	int		fd[2];
 	int		original_stdin;
+	t_ast	*cur;
+	t_ast	*par;
 	int		ret;
 
-	if (pipe(fd) == -1)
-		return (perror("pipe"), 1);
-	pid = fork();
-	reset_signals();
-	setup_execution_signals();
-	if (pid == -1)
-		return (perror("fork"), 1);
-	if (pid == 0)
-		handle_child_process(node, fd);
+	ret = 0;
+	cur = node;
 	original_stdin = dup(STDIN_FILENO);
-	close(fd[1]);
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[0]);
-	waitpid(pid, &status, 0);
-	ret = execute_ast(node->l, envp, store);
+	if (original_stdin == -1)
+		return (perror("dup"), 1);
+	while (cur->l && cur->l->type == TYP_REDHERE)
+		cur = cur->l;
+	while (cur && cur->type == TYP_REDHERE)
+	{
+		if (pipe(fd) == -1)
+			return (perror("pipe"), 1);
+		pid = fork();
+		reset_signals();
+		setup_execution_signals();
+		if (pid == -1)
+			return (perror("fork"), 1);
+		if (pid == 0)
+			handle_child_process(cur, fd);
+		int temp_stdin = dup(STDIN_FILENO);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		waitpid(pid, &status, 0);
+		if (cur == node)
+			break ;
+		par = node;
+		while (par->l != cur)
+			par = par->l;
+		cur = par;
+		dup2(temp_stdin, STDIN_FILENO);
+		close(temp_stdin);
+	}
+	while (node->l && node->l->type == TYP_REDHERE)
+		node = node->l;
+	if (node->l && node->l->type != TYP_REDHERE)
+		ret = execute_ast(node->l, envp, store);
 	dup2(original_stdin, STDIN_FILENO);
 	close(original_stdin);
 	return (ret);
