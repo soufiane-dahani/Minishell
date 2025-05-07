b284c755a6a14bb64254e9935cc61df0dfa86fa3
soufiane-dahani <dahanissoufiane@gmail.com>
2025-05-07 16:06:04 +0100

    norm 42

--- parsing/get_the_env.c
+++ parsing/get_the_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_the_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaait-am <yaait-am@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sodahani <sodahani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 15:21:32 by yaait-am          #+#    #+#             */
-/*   Updated: 2025/05/04 12:11:15 by yaait-am         ###   ########.fr       */
+/*   Updated: 2025/05/07 15:54:01 by sodahani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*my_getenv(char *s, char **copy)
 	i = 0;
 	while (env[i])
 	{
-		if (ft_strncmp(env[i], s, len) == 0)
+		if (ft_strncmp(env[i], s, len) == 0 && env[i][len] == '=')
 			return (env[i] + len + 1);
 		i++;
 	}
